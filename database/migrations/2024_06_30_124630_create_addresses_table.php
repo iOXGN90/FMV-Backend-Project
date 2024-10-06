ae339db26426849bfb2d801b6581ffbdce656842
OXGN <cabarrubias.nevinharold@gmail.com>
2024-10-06 15:35:09 +0800

    Update 2024_06_30_124630_create_addresses_table.php

--- database/migrations/2024_06_30_124630_create_addresses_table.php
+++ database/migrations/2024_06_30_124630_create_addresses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('street');
             $table->string('barangay');
+            $table->string('city');
             $table->integer('zip_code');
             $table->string('province');
             $table->timestamps();
